8b649e746902d29b7f333d490054c5cf90229a78
MAC User <macuser@COBATechs-MacBook-Air.local>
2024-03-07 17:44:28 -0600

    Added component

--- asu-botb-2024/src/App.jsx
+++ asu-botb-2024/src/App.jsx
@@ -1,216 +1,29 @@
-import React, { useState }  from 'react';
-import { Typography, AppBar, Card, TextField, Button, CardActions, CardContent, CardMedia, CssBaseline, Grid, Toolbar, Container} from '@mui/material';
-import { PhotoCamera } from '@mui/icons-material';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
+import React from 'react';
+import { CssBaseline, ThemeProvider, createTheme } from '@mui/material';
 import useStyles from './styles';
-import Box from '@mui/material/Box';
-import IconButton from '@mui/material/IconButton';
-import Menu from '@mui/material/Menu';
-import MenuIcon from '@mui/icons-material/Menu';
-import Avatar from '@mui/material/Avatar';
-import Tooltip from '@mui/material/Tooltip';
-import MenuItem from '@mui/material/MenuItem';
-import AdbIcon from '@mui/icons-material/Adb';
-import {Link } from '@mui/material';
-
-
-const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-    
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+// import ThreeScene from './components/ThreeScene'; // Assuming ThreeScene is another component you have set up
 
 const theme = createTheme();
-function Copyright() {
+
+function App() {
+  const classes = useStyles();
+
   return (
-    <Typography variant="body2" color="text.secondary" align="center">
-      {'Copyright © '}
-      <Link color="inherit" href="https://mui.com/">
-        Your Website
-      </Link>{' '}
-      {new Date().getFullYear()}
-      {'.'}
-    </Typography>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <div className= {classes.appContainer}>
+      <Navbar />
+      <main className={classes.mainContent}>
+        {/* Place for your main app content, which might include a Three.js scene or other elements */}
+        
+        {/* You can replace <ThreeScene /> with other components or content as needed */}
+      </main>
+      <Footer />
+      </div>
+    </ThemeProvider>
   );
-}
-const App = () => {
-
-    const pages = ['News', 'Scores', 'Schedule', 'Network', 'Teams', 'Stats', 'NFLGO'];
-    
-
-    const [anchorElNav, setAnchorElNav] = React.useState(null);
-    const [anchorElUser, setAnchorElUser] = React.useState(null);
-  
-    const handleOpenNavMenu = (event) => {
-      setAnchorElNav(event.currentTarget);
-    };
-    const handleOpenUserMenu = (event) => {
-      setAnchorElUser(event.currentTarget);
-    };
-  
-    const handleCloseNavMenu = () => {
-      setAnchorElNav(null);
-    };
-  
-    const handleCloseUserMenu = () => {
-      setAnchorElUser(null);
-    };
-  
-
-    const [text, setText] = useState('');
-
-    const handleSubmit = async (e) => {
-      e.preventDefault();
-      // Here you will call the API to convert text to video
-      console.log(text);
-    };
-    const classes = useStyles();
-    const logoUrl = 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.nfl.com%2F&psig=AOvVaw1_MKpkIhVcJ2Qq0X4b8vjB&ust=1709876737085000&source=images&cd=vfe&opi=89978449&ved=0CBMQjRxqFwoTCKD37a254YQDFQAAAAAdAAAAABAE';
-    return (
-        <ThemeProvider theme={theme}>
-       <>
-       <CssBaseline />
-       <AppBar position="static" sx={{ backgroundColor: '#001049' }}>
-      <Container maxWidth="xl">
-        <Toolbar disableGutters>
-          <Typography
-            variant="h6"
-            noWrap
-            component="a"
-            href="#app-bar-with-responsive-menu"
-            sx={{
-              mr: 2,
-              display: { xs: 'none', md: 'flex' },
-              fontFamily: 'monospace',
-              fontWeight: 700,
-              letterSpacing: '.3rem',
-              color: 'inherit',
-              textDecoration: 'none',
-            }}
-          >
-            NFL+
-          </Typography>
-
-          <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
-            <IconButton
-              size="large"
-              aria-label="account of current user"
-              aria-controls="menu-appbar"
-              aria-haspopup="true"
-              onClick={handleOpenNavMenu}
-              color="inherit"
-            >
-              <MenuIcon />
-            </IconButton>
-            <Menu
-              id="menu-appbar"
-              anchorEl={anchorElNav}
-              anchorOrigin={{
-                vertical: 'bottom',
-                horizontal: 'left',
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: 'top',
-                horizontal: 'left',
-              }}
-              open={Boolean(anchorElNav)}
-              onClose={handleCloseNavMenu}
-              sx={{
-                display: { xs: 'block', md: 'none' },
-              }}
-            >
-              {pages.map((page) => (
-                <MenuItem key={page} onClick={handleCloseNavMenu}>
-                  <Typography textAlign="center">{page}</Typography>
-                </MenuItem>
-              ))}
-            </Menu>
-          </Box>
-          <AdbIcon sx={{ display: { xs: 'flex', md: 'none' }, mr: 1 }} />
-          {/* <Typography
-            variant="h5"
-            noWrap
-            component="a"
-            href="#app-bar-with-responsive-menu"
-            sx={{
-              mr: 2,
-              display: { xs: 'flex', md: 'none' },
-              flexGrow: 1,
-              fontFamily: 'monospace',
-              fontWeight: 700,
-              letterSpacing: '.3rem',
-              color: 'inherit',
-              textDecoration: 'none',
-            }}
-          >
-            LOGO
-          </Typography> */}
-          <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
-            {pages.map((page) => (
-              <Button
-                key={page}
-                onClick={handleCloseNavMenu}
-                sx={{ my: 2, color: 'white', display: 'block' }}
-              >
-                {page}
-              </Button>
-            ))}
-          </Box>
-
-          <Box sx={{ flexGrow: 0 }}>
-            <Menu
-              sx={{ mt: '45px' }}
-              id="menu-appbar"
-              anchorEl={anchorElUser}
-              anchorOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
-              }}
-              open={Boolean(anchorElUser)}
-              onClose={handleCloseUserMenu}
-            >
-            </Menu>
-          </Box>
-        </Toolbar>
-      </Container>
-    </AppBar>
-         <main>
-            <div className= {classes.container}>
-                
-            </div>
-         </main>
-         <Box
-    sx={{
-      display: 'flex',
-      flexDirection: 'column',
-      minHeight: '100vh',
-    }}
-  >
-    <CssBaseline />
-    
-    <Box
-      component="footer"
-      sx={{
-        py: 3,
-        px: 2,
-        mt: 'auto',
-        backgroundColor:'#002da1'
-      }}
-    >
-      <Container maxWidth="sm">
-        <Typography variant="body1" align="center">
-          2024 Battle Of The Brains, All Rights Reserved.
-        </Typography>
-        <Copyright />
-      </Container>
-    </Box>
-  </Box>
-       </>
-       </ThemeProvider>
-    )
 }
 
 export default App;
