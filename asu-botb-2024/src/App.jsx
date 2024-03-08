5f5a954fb8531c4e3f31caf69f4637c3302deb09
user <user@Oluwatosins-MacBook-Pro.local>
2024-03-08 02:32:08 -0600

    The Horizontal scroll

--- asu-botb-2024/src/App.jsx
+++ asu-botb-2024/src/App.jsx
@@ -3,6 +3,7 @@ import { CssBaseline, ThemeProvider, createTheme } from '@mui/material';
 import useStyles from './styles';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
+import HorizontalScroll from './components/Horizontalscroll';
 
 const theme = createTheme();
 
@@ -12,14 +13,28 @@ function App() {
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
+
       <div className= {classes.appContainer}>
       <Navbar />
       <main className={classes.mainContent}>
-
+      <div className="App">
+      <h1>Horizontal Scroll Example</h1>
+      <HorizontalScroll>
+        {/* Your content goes here */}
+        <div className="item">
+          Item 1
+          <img src="" alt="" />
+        </div>
+        <div className="item">Item 2</div>
+        <div className="item">Item 3</div>
+        {/* Add more items as needed */}
+      </HorizontalScroll>
+    </div>
         
       </main>
       <Footer />
       </div>
+      
     </ThemeProvider>
   );
 }
--- asu-botb-2024/src/components/Navbar.jsx
+++ asu-botb-2024/src/components/Navbar.jsx
@@ -56,5 +56,5 @@ function Navbar() {
     </AppBar>
   );
 }
-
 export default Navbar;
+
